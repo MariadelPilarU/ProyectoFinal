1d5dba5011af1fe746733f15dd0fd2149a382cbf
María del Pilar Urcola <ma.delpilar.u@gmail.com>
2023-07-06 21:14:35 -0300

    6-7-23

--- js/main.js
+++ js/main.js
@@ -92,11 +92,7 @@
     });
   });
 
-  // jQuery counterUp (used in Whu Us section)
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
+
 
   // Porfolio isotope and filter
   $(window).on('load', function () {
@@ -111,13 +107,6 @@
     });
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
 
 })(jQuery);
 
